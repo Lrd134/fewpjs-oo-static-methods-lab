67f5b2b78db6e64df78c7d694ac6b44d26f05f5d
Lrd134 <larryc3200@gmail.com>
2021-09-25 14:30:26 -0400

    commit tree f13dfdec2d07e6ab4777c66b7f38a8fdd562b34e

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize (string) {
+    return `${string[0].toUpperCase()}${string.slice(1)}`;
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(string) {
+    const nonTitlecasedWords = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ];
+    return string.split(" ").map(
+      (currentWord, index) => {
+      if (!nonTitlecasedWords.find(e => {
+        return e === currentWord;
+      }) || index === 0) {
+        debugger;
+        currentWord = `${currentWord[0].toUpperCase()}${currentWord.slice(1)}`;
+      }
+      return currentWord;
+    }).join(" ");
+  }
 }
